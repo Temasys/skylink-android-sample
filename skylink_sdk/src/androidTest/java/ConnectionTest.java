872f634e61647b4c7cec547fb356f9856341ada6
Janidu <janidu.wanigasuriya@temasys.com.sg>
2015-01-12 19:27:12 +0800

    #AND-60 Modified the androidTestCompile dependencies

--- skylink_sdk/src/androidTest/java/ConnectionTest.java
+++ skylink_sdk/src/androidTest/java/ConnectionTest.java
@@ -1,22 +1,20 @@
-package sg.com.temasys.skylink.sdk;
+import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import static junit.framework.Assert.assertTrue;
-
-
 /**
  * Created by janidu on 12/1/15.
  */
+
 @Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
-public class ExampleTest {
+public class ConnectionTest {
 
     @Test
-    public void example() {
-        assertTrue(true);
+    public void testConnection() {
+        Assert.assertTrue(false);
     }
 }
