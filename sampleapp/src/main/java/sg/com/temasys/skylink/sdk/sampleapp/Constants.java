c0878d199b4aad41d00b128912fe0c6971a6742e
Lavanya Sudharsanam <lavanya@temasys.com.sg>
2015-01-29 19:10:24 +0800

    Remove Unused Constants
    AND-24

--- sampleapp/src/main/java/sg/com/temasys/skylink/sdk/sampleapp/Constants.java
+++ sampleapp/src/main/java/sg/com/temasys/skylink/sdk/sampleapp/Constants.java
@@ -1,9 +1,7 @@
 package sg.com.temasys.skylink.sdk.sampleapp;
 
 public final class Constants {
-    public static final String ROOM_NAME = "room";
     public static final int DURATION = 20000;
-    public static final String MY_USER_NAME = "userAudio";
     public static final int TIME_OUT = 60;
 
     private Constants() {
