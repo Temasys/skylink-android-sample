97d976ae494bfa080bf50d61f738cb5a35c68ef4
Janidu <janidu.wanigasuriya@temasys.com.sg>
2015-01-30 18:00:14 +0800

    Added custom javadoc stylesheet
    AND-99

--- skylink_sdk/src/main/java/sg/com/temasys/skylink/sdk/rendering/VideoRendererGuiListener.java
+++ skylink_sdk/src/main/java/sg/com/temasys/skylink/sdk/rendering/VideoRendererGuiListener.java
@@ -3,9 +3,6 @@ package sg.com.temasys.skylink.sdk.rendering;
 import android.graphics.Point;
 import android.opengl.GLSurfaceView;
 
-/**
- * Created by janidu on 22/1/15.
- */
 public interface VideoRendererGuiListener {
     void updateDisplaySize(GLSurfaceView surface, Point screenDimensions);
 }
