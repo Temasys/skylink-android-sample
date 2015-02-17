8331126297ad895a68aee0b317727c31f82a7418
Janidu <janidu.wanigasuriya@temasys.com.sg>
2015-02-17 13:43:28 +0800

    Modified the SDK sample app to connect to room using a connection string
    AND-153, AND-154

--- sampleapp/src/main/java/sg/com/temasys/skylink/sdk/sampleapp/Utils.java
+++ sampleapp/src/main/java/sg/com/temasys/skylink/sdk/sampleapp/Utils.java
@@ -1,13 +1,29 @@
 package sg.com.temasys.skylink.sdk.sampleapp;
 
+import android.util.Base64;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 public class Utils {
+
+    private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+    private static final String TAG = Utils.class.getCanonicalName();
 
     private Utils() {
     }
 
-    public static void setRoomDetails(boolean isPeerInRoom, TextView textView, String peerName,String roomName,String userName) {
+    public static void setRoomDetails(boolean isPeerInRoom, TextView textView,
+                                      String peerName, String roomName, String userName) {
         String roomDetails = "Room Name : " + roomName
                 + "\nYou are signed in as : " + userName + "\n";
         if (isPeerInRoom) {
@@ -16,5 +32,90 @@ public class Utils {
             roomDetails += "You are alone in this room";
         }
         textView.setText(roomDetails);
+    }
+
+    /**
+     * Returns the SkylinkConnectionString
+     *
+     * @param roomName  Name of the room
+     * @param apiKey    API Key
+     * @param secret    API secret
+     * @param startTime Room Start Time in GMT
+     * @param duration  Duration of the room in Hours
+     * @return
+     */
+    public static String getSkylinkConnectionString(String roomName, String apiKey,
+                                                    String secret,
+                                                    Date startTime, int duration) {
+
+        Log.d(TAG, "Room name " + roomName);
+        Log.d(TAG, "API Key " + apiKey);
+        Log.d(TAG, "startTime " + startTime);
+        Log.d(TAG, "duration " + duration);
+
+        // Convert the date in to ISO format
+        String dateString = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:00.0'Z'")
+                .format(startTime);
+
+        // Compute RFC 2104-compliant HMAC signature
+        String cred = calculateRFC2104HMAC(roomName + "_" + duration + "_"
+                + dateString, secret);
+        try {
+            cred = URLEncoder.encode(cred, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+
+        return apiKey + "/"
+                + roomName + "/" + dateString + "/" + duration + "?cred="
+                + cred;
+    }
+
+    /**
+     * Computes RFC 2104-compliant HMAC signature.
+     *
+     * @param data data The data to be signed.
+     * @param key  The signing key.
+     * @return The Base64-encoded RFC 2104-compliant HMAC signature.
+     */
+    public static String calculateRFC2104HMAC(String data, String key) {
+        String result = null;
+        try {
+
+            // Get an hmac_sha1 key from the raw key bytes
+            SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(),
+                    HMAC_SHA1_ALGORITHM);
+
+            // Get an hmac_sha1 Mac instance and initialize with the signing key
+            Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
+            mac.init(signingKey);
+
+            // Compute the hmac on input data bytes
+            byte[] rawHmac = mac.doFinal(data.getBytes());
+
+            // Base64-encode the hmac
+            result = Base64
+                    .encodeToString(rawHmac, android.util.Base64.DEFAULT);
+
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to generate HMAC : " + e.getMessage(), e);
+        }
+        return result.substring(0, result.length() - 1);
+    }
+
+    /**
+     * Converts a time stamp in local time zone to GMT
+     *
+     * @param timestamp
+     * @return
+     */
+    public static Date convertTimeStampToGMT(long timestamp) {
+        Calendar calendar = Calendar.getInstance();
+        TimeZone tz = calendar.getTimeZone();
+
+        int tzt = tz.getOffset(System.currentTimeMillis());
+        timestamp -= tzt;
+        calendar.setTimeInMillis(timestamp);
+        return calendar.getTime();
     }
 }
