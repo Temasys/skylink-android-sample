fed6fb2ee59efda157257946ce4cd52970dd8ae1
Janidu <janidu.wanigasuriya@temasys.com.sg>
2015-04-14 17:29:19 +0800

    Implemented UserInfo
    AND-225
    
    - Added options to set video height, width and fps through configuration
    - Modified the capturer to make use of these configurations
    - Added a getUserInfo method to retreive user information
    - Added a configuration to use stereo audio
    - Added unit tests

--- skylink_sdk/src/androidTest/java/sg/com/temasys/skylink/sdk/rtc/SkylinkConnectionTest.java
+++ skylink_sdk/src/androidTest/java/sg/com/temasys/skylink/sdk/rtc/SkylinkConnectionTest.java
@@ -7,7 +7,11 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowLog;
 
+import sg.com.temasys.skylink.sdk.config.SkylinkConfig;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -24,6 +28,80 @@ public class SkylinkConnectionTest {
     @Before
     public void setUp() throws Exception {
         ShadowLog.stream = System.out;
+    }
+
+    @Test
+    public void testVideoWidthDefault() {
+        SkylinkConfig skylinkConfig = new SkylinkConfig();
+        assertEquals(skylinkConfig.getVideoWidth(), SkylinkConfig.MAX_VIDEO_WIDTH);
+    }
+
+    @Test
+    public void testVideoHeightDefault() {
+        SkylinkConfig skylinkConfig = new SkylinkConfig();
+        assertEquals(skylinkConfig.getVideoHeight(), SkylinkConfig.MAX_VIDEO_HEIGHT);
+    }
+
+    @Test
+    public void testVideoHeight() {
+        SkylinkConfig skylinkConfig = new SkylinkConfig();
+        skylinkConfig.setVideoHeight(200);
+
+        SkylinkConfig newConfig = new SkylinkConfig(skylinkConfig);
+        assertEquals(newConfig.getVideoHeight(), 200);
+    }
+
+    @Test
+    public void testVideoHeightMax() {
+        SkylinkConfig skylinkConfig = new SkylinkConfig();
+        skylinkConfig.setVideoHeight(20000);
+
+        SkylinkConfig newConfig = new SkylinkConfig(skylinkConfig);
+        assertEquals(newConfig.getVideoHeight(), SkylinkConfig.MAX_VIDEO_HEIGHT);
+    }
+
+    @Test
+    public void testVideoWidth() {
+        SkylinkConfig skylinkConfig = new SkylinkConfig();
+        skylinkConfig.setVideoWidth(200);
+
+        SkylinkConfig newConfig = new SkylinkConfig(skylinkConfig);
+        assertEquals(newConfig.getVideoWidth(), 200);
+    }
+
+    @Test
+    public void testVideoWidthMax() {
+        SkylinkConfig skylinkConfig = new SkylinkConfig();
+        skylinkConfig.setVideoWidth(20000);
+
+        SkylinkConfig newConfig = new SkylinkConfig(skylinkConfig);
+        assertEquals(newConfig.getVideoWidth(), SkylinkConfig.MAX_VIDEO_WIDTH);
+    }
+
+    @Test
+    public void testAudioStereoDefault() {
+        SkylinkConfig skylinkConfig = new SkylinkConfig();
+        assertTrue(skylinkConfig.isStereoAudio());
+    }
+
+    @Test
+    public void testAudioStereoTrue() {
+        SkylinkConfig skylinkConfig = new SkylinkConfig();
+        skylinkConfig.setStereoAudio(true);
+        assertTrue(skylinkConfig.isStereoAudio());
+    }
+
+    @Test
+    public void testVideoFPSDefault() {
+        SkylinkConfig skylinkConfig = new SkylinkConfig();
+        assertEquals(skylinkConfig.getVideoFps(), SkylinkConfig.MAX_VIDEO_FPS);
+    }
+
+    @Test
+    public void testVideoFPSMax() {
+        SkylinkConfig skylinkConfig = new SkylinkConfig();
+        skylinkConfig.setVideoFps(4000);
+        assertEquals(new SkylinkConfig(skylinkConfig).getVideoFps(), SkylinkConfig.MAX_VIDEO_FPS);
     }
 
     @Test
@@ -51,5 +129,4 @@ public class SkylinkConnectionTest {
         skylinkConnection.runOnUiThread(mockRunnable);
         verify(mockRunnable).run();
     }
-
 }
