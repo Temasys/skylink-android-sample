1bd281292f563f6fb8a2c389ad0b56034508558b
Muoi Pham <muoi.pham@temasys.com.sg>
2018-07-26 12:34:25 +0800

    ENH: Improve GUI of sample app
    AND-919
    
    Improve GUI of the sample app in order make it easier to use and look prettier
    
    Major changes:
    
    Add menu about on action bar in Settings activity in order to provide build configuration info

--- sampleapp/src/main/java/sg/com/temasys/skylink/sdk/sampleapp/ConfigFragment/ConfigActivity.java
+++ sampleapp/src/main/java/sg/com/temasys/skylink/sdk/sampleapp/ConfigFragment/ConfigActivity.java
@@ -1,11 +1,13 @@
 package sg.com.temasys.skylink.sdk.sampleapp.ConfigFragment;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import sg.com.temasys.skylink.sdk.sampleapp.BuildConfig;
 import sg.com.temasys.skylink.sdk.sampleapp.R;
-import sg.com.temasys.skylink.sdk.sampleapp.data.model.PermRequesterInfor;
+import sg.com.temasys.skylink.sdk.sampleapp.utils.Utils;
 
 public class ConfigActivity extends AppCompatActivity {
 
@@ -47,5 +49,26 @@ public class ConfigActivity extends AppCompatActivity {
 
         //Save the fragment's instance
         getSupportFragmentManager().putFragment(outState, CONFIG_FRAGMENT_TAG, mConfigFragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.about, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_about) {
+            String log = "SDK Version: " + sg.com.temasys.skylink.sdk.BuildConfig.VERSION_NAME
+                    + "\n" + "Sample application version: " + BuildConfig.VERSION_NAME;
+            Utils.toastLogLong(CONFIG_FRAGMENT_TAG, this, log);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
